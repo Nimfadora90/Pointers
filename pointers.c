ee7067f3b656ce3c164efd483db4cc95a5d3f300
Nimfadora90 <georgoudi.el@gmail.com>
2020-04-05 09:24:34 -0400

    Reading input from command line.

--- pointers.c
+++ pointers.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void swap (char *num1, char *num2);
+void swap (int *num1, int *num2);
 
-int main (int argc, char *argv){
-  char x = argv[1];
-  char y = argv[2];
+int main (int argc, char *argv[]){
+  int x = atoi(argv[1]);
+  int y = atoi(argv[2]);
 
   printf("x is %d, y is %d\n", x, y);
   swap (&x,&y);
   printf("x is %d, y is %d\n", x, y);
   return 0;
 }
-void swap (char *num1, char *num2){
-  char temp;
-temp = *num1;
-*num1 = *num2;
-*num2 = temp;
+
+void swap (int *num1, int *num2){
+  int temp;
+  temp = *num1;
+  *num1 = *num2;
+  *num2 = temp;
 }
